b12db5c246a8f5dffcf51e93d844be1f022a6559
libienz <leekh10195@gmail.com>
2024-02-13 20:29:24 +0900

    feat: 입력 문자열 Splitter 구현

---
+++ src/main/java/org/example/view/InputSplitter.java
@@ -0,0 +1,32 @@
+package org.example.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputSplitter {
+    private String delimiter = ",|:";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+
+    public List<String> splitToOperands(String target) {
+        return Arrays.stream(target.split(delimiter)).toList();
+    }
+
+    private boolean isCustomDelimiterExist(String input) {
+        final Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
+        if (m.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    private String scanCustomDelimiter(String input) {
+        final Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
+        return m.group(1);
+    }
+
+    private void addDelimiter(String input) {
+        delimiter += scanCustomDelimiter(input);
+    }
+}
