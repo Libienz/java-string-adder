0e956180bf7a0bbda9a4c52e5a66feb4ad9c46ad
libienz <leekh10195@gmail.com>
2024-02-13 21:12:14 +0900

    feat: 입력 변환 기능 구현

---
+++ src/main/java/org/example/view/InputMapper.java
@@ -0,0 +1,38 @@
+package org.example.view;
+
+import org.example.domain.Operand;
+import org.example.domain.Operands;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class InputMapper {
+
+    private final InputSplitter inputSplitter;
+
+    public InputMapper(InputSplitter inputSplitter) {
+        this.inputSplitter = inputSplitter;
+    }
+
+    public Operands mapToOperands(String input) {
+        List<String> operands = inputSplitter.splitToOperands(input);
+        return Operands.from(operands.stream()
+                .map(this::mapToInt)
+                .map(Operand::from)
+                .toList());
+    }
+
+    private Integer mapToInt(String operand) {
+        try {
+            return Integer.parseInt(operand);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private Operand mapToOperand(Integer operand) {
+        return Operand.from(operand);
+    }
+}
