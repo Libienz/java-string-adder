82baec70040dc8e1753bf86331bf8f45be654adf
libienz <leekh10195@gmail.com>
2024-02-13 19:24:37 +0900

    feat: Operands 도메인 클래스 속성 정의

---
+++ src/main/java/org/example/domain/Operands.java
@@ -0,0 +1,12 @@
+package org.example.domain;
+
+import java.util.List;
+
+public class Operands {
+
+    private final List<Operand> operands;
+
+    public Operands(List<Operand> operands) {
+        this.operands = operands;
+    }
+}
