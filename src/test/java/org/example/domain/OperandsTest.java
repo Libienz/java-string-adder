204999ac2f8336a298febffbfee6aa135e8c8845
libienz <leekh10195@gmail.com>
2024-02-13 21:25:25 +0900

    test: Operands 도메인 덧셈 기능 테스트 작성

---
+++ src/test/java/org/example/domain/OperandsTest.java
@@ -0,0 +1,24 @@
+package org.example.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("피연산자 일급 컬렉션 도메인 테스트")
+class OperandsTest {
+
+    @DisplayName("덧셈 테스트")
+    @Test
+    void testSummation() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Operands operands = Operands.from(numbers.stream()
+                .map(Operand::from)
+                .toList());
+        assertThat(operands.calculateSummation()).isEqualTo(21);
+    }
+}
