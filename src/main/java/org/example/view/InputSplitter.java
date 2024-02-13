2344438c40c3daec850262af5b8277b3f7cae056
libienz <leekh10195@gmail.com>
2024-02-13 21:11:07 +0900

    fix: 커스텀 구분자 파악 후 문자열에서 제거하도록 수정

--- src/main/java/org/example/view/InputSplitter.java
+++ src/main/java/org/example/view/InputSplitter.java
@@ -8,25 +8,35 @@ import java.util.regex.Pattern;
 public class InputSplitter {
     private String delimiter = ",|:";
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final String REMOVE_PATTERN = "//.*\n";
 
     public List<String> splitToOperands(String target) {
-        return Arrays.stream(target.split(delimiter)).toList();
-    }
-
-    private boolean isCustomDelimiterExist(String input) {
-        final Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
-        if (m.find()) {
-            return true;
-        }
-        return false;
+        String customDelimiter = scanCustomDelimiter(target);
+        addDelimiter(customDelimiter);
+        String operands = removeCustomDelimiterPattern(target);
+        return Arrays.stream(operands.split(delimiter)).toList();
     }
 
     private String scanCustomDelimiter(String input) {
         final Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
-        return m.group(1);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return "";
     }
 
-    private void addDelimiter(String input) {
-        delimiter += "|" + scanCustomDelimiter(input);
+    private void addDelimiter(String customDelimiter) {
+        if (customDelimiter == "") {
+            return;
+        }
+        delimiter += "|" + customDelimiter;
+    }
+
+    private String removeCustomDelimiterPattern(String input) {
+        final Matcher m = Pattern.compile(REMOVE_PATTERN).matcher(input);
+        if (m.find()) {
+            return m.replaceAll("");
+        }
+        return input;
     }
 }
